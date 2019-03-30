b0eaed08ecf2859821c9e4a0526f83e661224386
Jennifer Parra <jparra.biblio@gmail.com>
2019-03-30 03:55:30 -0300

    lista pantalla inicial y pantalla de chatbot

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+window.onload = () =>{
+    document.getElementById('btn-access').addEventListener('click', ()=>{
+        document.getElementById('index').style.display='none';
+        document.getElementById('talking').style.display='block';
+
+    })
+
+    /* document.getElementById('btn-access').addEventListener('click', ()=>{
+        
+    }) */
+
+
+
+
+}
